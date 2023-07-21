d78f510bcddeb0118286d805b6a5b6451883bc8f
Peu.Majumder <peumajumder94@gmail.com>
2023-07-21 21:46:01 +0530

    episode-02

---
+++ App.js
@@ -0,0 +1,16 @@
+const parent = React.createElement("h1", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am a h1 tag"),
+    React.createElement("h2", {}, "I am a h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am a h1 tag"),
+    React.createElement("h2", {}, "I am a h2 tag"),
+  ]),
+]);
+
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
